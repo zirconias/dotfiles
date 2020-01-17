11e263fcec4714643fce93f89467548e63bfa422
ztallion <ztallion@ztallion.local>
2020-01-17 14:11:54 +0100

    wip

--- fedora/bootstrap.sh
+++ fedora/bootstrap.sh
@@ -8,3 +8,11 @@ readonly HOSTNAME_FILE="/etc/hostname"
 readonly FEDORA_VERSION=$(rpm -E %fedora)
 readonly CPU_ARCH=$(uname -p)
 
+
+
+
+#Configure rpm fusion
+sudo dnf install \
+  "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+  "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
